52246b702d20bb50a2363dfd6b43a15ca2f5ab83
Sentinel297 <ledodoarobase@gmail.com>
2026-02-01 20:48:14 +0100

    added source code

---
+++ Source/BernaelHelper/Harmony.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using HarmonyLib;
+using EBSGFramework;
+using RimWorld;
+using System.Reflection;
+
+namespace Bernael_Xenotype
+{
+    [StaticConstructorOnStartup]
+    public static class BernaelGraceHarmony
+    {
+        static BernaelGraceHarmony()
+        {
+            Harmony harmony = new Harmony("BernaelXenotype.Harmony.SentinelPatches");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(ResourceGene), nameof(ResourceGene.Tick))]
+        public static class Patch_Grace
+        {
+            static bool Prepare()
+            {
+                return ModsConfig.IsActive("Sov.Nephilim");
+            }
+
+            public static void Postfix(ref ResourceGene __instance)
+            {
+                Pawn pawn = __instance.pawn;
+                if (!__instance.pawn.IsHashIntervalTick(180)|| __instance.def != BernaelDefOf.GS_Grace_New ||
+                    __instance.Resource.Value > 0 || !pawn.InMentalState) return;
+
+                pawn.MorbOut(BernaelDefOf.BX_Bernael);
+            }
+        }
+
+        [HarmonyPatch(typeof(IngestionOutcomeDoer), nameof(IngestionOutcomeDoer.DoIngestionOutcome))]
+        public static class Patch_GraceIngestion
+        {
+            static bool Prepare()
+            {
+                return ModsConfig.IsActive("Sov.Nephilim");
+            }
+
+            public static void Postfix(Pawn pawn, Thing ingested)
+            {
+                if (pawn.Map == null || ingested.def.GetGraceOutcomeDoer() == null) return;
+                foreach (Pawn otherPawn in pawn.Map.mapPawns.AllPawnsSpawned)
+                {
+                    if (otherPawn.genes == null || otherPawn.relations == null) continue;
+                    bool hasHateGene = otherPawn.genes.GetGene(BernaelDefOf.BX_Grace_Galling) != null;
+                    if (!hasHateGene) continue;
+
+                    var memory = otherPawn.needs.mood.thoughts;
+                    if (memory == null) continue;
+
+                    TaleRecorder.RecordTale(BernaelDefOf.BX_ConsumedGrace, pawn, otherPawn);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Pawn_InteractionsTracker), nameof(Pawn_InteractionsTracker.TryInteractWith))]
+        public static class Patch_WeirdSpeech
+        {
+            public static FieldInfo pawnField => AccessTools.Field(typeof(Pawn_InteractionsTracker), "pawn");
+            public static void Postfix(Pawn_InteractionsTracker __instance, ref bool __result, Pawn recipient)
+            {
+                if (!__result ) return;
+                Pawn instigator = (Pawn)pawnField.GetValue(__instance);
+                if (instigator.genes == null || instigator.genes.GetGene(BernaelDefOf.BX_DarkSpeech) == null) return;
+                if (recipient.needs.mood.thoughts == null || recipient.genes == null || recipient.genes.GetGene(BernaelDefOf.BX_DarkSpeech) != null) return;
+                recipient.needs.mood.thoughts.memories.TryGainMemory(BernaelDefOf.BX_HeardDarkSpeech);
+            }
+        }
+    }
+}
---
+++ Source/DefOf.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Bernael_Xenotype
+{
+    [DefOf]
+    public class BernaelDefOf
+    {
+        public BernaelDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(BernaelDefOf));
+        }
+
+        public static GeneDef BX_Grace_Galling;
+        public static GeneDef BX_GracePendulum;
+        public static GeneDef BX_DarkSpeech;
+        public static TaleDef BX_ConsumedGrace;
+        public static XenotypeDef BX_Bernael;
+        public static HediffDef BX_Mutation_Hediff;
+        public static ThoughtDef BX_ConsumedGraceThoughtMood;
+        public static ThoughtDef BX_HeardDarkSpeech;
+
+        [MayRequire("Sov.Nephilim")]
+        public static GeneDef GS_Grace_New;
+        [MayRequire("Sov.Nephilim")]
+        public static XenotypeDef GS_Nephilim;
+    }
+}
